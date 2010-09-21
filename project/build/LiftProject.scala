23a61ec106bd31a18ea1d6bff3e3c765f39a92bb
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2010-09-21 19:45:31 +1000

    Updated to Scala 2.8 and removed H2 dep

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,16 +1,15 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.0"
+  val liftVersion = "2.1-RC2"
   
   lazy val artifactory = "Artifactory Release" at "http://hyperion:9080/artifactory/libs-releases"   
   
   override def libraryDependencies = Set(
-    "net.liftweb" % "lift-webkit" % liftVersion % "compile->default",
-    "net.liftweb" % "lift-common" % liftVersion % "compile->default",
-    "net.liftweb" % "lift-mapper" % liftVersion % "compile->default",
-    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
-    "com.h2database" % "h2" % "1.2.138"
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-common" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
     ) ++ super.libraryDependencies  
   
 }
